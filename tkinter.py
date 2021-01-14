91139795e17755f6b9db6e30d0920b8192dc767f
codinglah <codinglah@mail.com>
2021-01-14 14:44:15 +0800

    Update tkinter.py
    A few improvements.

--- tkinter.py
+++ tkinter.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import tkinter as tk
+import tkinter.messagebox as box
 window = tk.Tk() 
 window.title('Select Calculation')
 window.pack(padx = 0, pady = 100)
@@ -9,7 +10,7 @@ button1.configure(text = 'Mean', command = file)
 button1.pack(side = LEFT)
 button2 = tk.Button(window)
 button2.configure(text = 'Median', command = file)
-button2.pack(side = MIDDLE)
+button2.pack()
 button3 = tk.Button(window)
 button3.configure(text = 'Mode', command = file)
 button3.pack(side = RIGHT)
@@ -22,4 +23,5 @@ def file():
     button4.pack(side = RIGHT)
     frame = tk.Frame(window2)
     entry = tk.Entry(frame)
-    
+    if not entry.get() == '*.xlsx' or not entry.get() == '*.csv' or not entry.get() == '*.xls':
+        box.showerror('No Support', 'File type not supported')
